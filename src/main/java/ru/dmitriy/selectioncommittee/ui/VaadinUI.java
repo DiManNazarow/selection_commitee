1351500e6e1eece049c85f8b369e115f790cc724
Дмитрий <dmitriy.nazarov@altarix.ru>
2017-03-17 01:02:33 +0300

    create main vaadin page

--- src/main/java/ru/dmitriy/selectioncommittee/ui/VaadinUI.java
+++ src/main/java/ru/dmitriy/selectioncommittee/ui/VaadinUI.java
@@ -3,9 +3,7 @@ package ru.dmitriy.selectioncommittee.ui;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 
 /**
  * Created by Dmitriy Nazarow on 17.03.17.
@@ -17,6 +15,20 @@ public class VaadinUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Selection"));
+       buildUI();
+    }
+
+    private void buildUI(){
+        VerticalLayout mainLayout = new VerticalLayout();
+        Label label = new Label("Добро пожаловать на сайт приемной комиссиb!");
+        mainLayout.addComponent(label);
+        mainLayout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+        Button continueButton = new Button("Продолжить");
+        continueButton.addClickListener(l -> {
+            Notification.show("В разработке. Совсем скоро!");
+        });
+        mainLayout.addComponent(continueButton);
+        mainLayout.setComponentAlignment(continueButton, Alignment.BOTTOM_CENTER);
+        setContent(mainLayout);
     }
 }
